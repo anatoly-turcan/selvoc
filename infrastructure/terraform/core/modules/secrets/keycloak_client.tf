de14cb9409d886c2a6ef18bb883afc684fe558a7
Anatoly Turcan <anatoly8293@gmail.com>
2025-05-28 08:54:10 +0300

    chore(infra): generate secrets for each service

---
+++ infrastructure/terraform/core/modules/secrets/keycloak_client.tf
@@ -0,0 +1,11 @@
+resource "aws_secretsmanager_secret" "keycloak_client_secret" {
+  for_each = toset(local.services_with_keycloak_client_secret)
+
+  name                    = "${var.project_name}/${var.environment}/service/${each.key}/keycloak-client-secret"
+  recovery_window_in_days = 0
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+  }
+}
--- infrastructure/terraform/core/modules/secrets/locals.tf
+++ infrastructure/terraform/core/modules/secrets/locals.tf
@@ -1,4 +1,8 @@
 locals {
+  services_with_postgres               = ["keycloak", "user", "chat"]
+  services_with_rabbitmq               = ["keycloak", "user", "chat"]
+  services_with_keycloak_client_secret = ["user", "chat"]
+
   postgres_username = "postgres_${random_string.postgres_username_suffix.result}"
   rabbitmq_username = "admin_${random_string.rabbitmq_username_suffix.result}"
 }
