ea5f1a662a4941184fad15444ad2f5d288a251c8
Anatoly Turcan <anatoly8293@gmail.com>
2025-05-28 09:11:29 +0300

    chore(cicd): add user service helm and workflow

--- backend/user/src/config/typeorm.config.ts
+++ backend/user/src/config/typeorm.config.ts
@@ -11,9 +11,9 @@ export const typeormConfig: TypeormConfig = {
   username: get('POSTGRES_USERNAME').required().asString(),
   password: get('POSTGRES_PASSWORD').required().asString(),
   database: get('POSTGRES_DATABASE').required().asString(),
-  // ssl: {
-  //   rejectUnauthorized: false, // TODO: fix
-  // },
+  ssl: {
+    rejectUnauthorized: false, // TODO: fix
+  },
   migrationsRun: true,
   synchronize: false,
   logging: ['error', 'migration', 'schema'],
